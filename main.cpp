8b703938c0de27a7f080a5dcf6038ab8be711dcc
Cole Palmore <cpalmore@eisinc.com>
2024-11-30 20:39:56 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// http://www.6502.org/users/obelisk/
+// http://www.6502.org/users/obelisk/electronics/sb-6502.html
+
+
+
+struct CPU {
+    using Byte = unsigned char;
+    using Word = unsigned short;
+
+    Word PC;    // program Counter
+    Word SP;    // Stack Counter
+
+    Byte A,X,Y; //Registers
+
+    Byte C : 1; // Carry Flag
+    Byte Z : 1; // Zero Flag
+    Byte I : 1; // Interrupt Disable
+    Byte D : 1; // Decimal Mode
+    Byte B : 1; // Break Mode
+    Byte O : 1; // Overflow Flag
+    Byte N : 1; // Negative Flag
+    
+     
+
+};
+
+
+int main(){
+
+    return 0;
+}
